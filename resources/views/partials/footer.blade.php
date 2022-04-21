7002b1fcf4fae13f57d8f62c0069067d2abe92bf
stefanom13 <stefanomancuso13@gmail.com>
2022-04-21 15:35:02 +0200

    footer inserito

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,17 @@
-<footer>
-   footer
+<footer >
+   <div class="container dflexFooter">
+
+         <button>
+            sign-up now!
+         </button>
+
+         <div class="contIconsFooter">
+            <p>follow us</p>
+            <ul>
+               <li>icone</li>
+               
+            </ul>
+         </div>
+     
+   </div>
 </footer>
