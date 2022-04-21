ce9475dfc5f0d1d03d8335f97075e007891b41e2
stefanom13 <stefanomancuso13@gmail.com>
2022-04-21 10:53:41 +0200

    inserimento header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,14 @@
 <header>
     <div>
-        <div>
-            header
+        <div class="container">
+            <div>
+                barr blu
+            </div>
+            {{-- logo --}}
+            <figure class="logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="">
+            </figure>
+
         </div>
     </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+// Route::get('/about', function () {
+//     return view('about');
+// })->name('about');
